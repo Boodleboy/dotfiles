6ad6e9ca68cf14da8001ce97ffe9b8908c76f70d
Jake <jacobmcpeak98@gmail.com>
2024-06-08 08:52:57 -0400

    Add git-fugitive

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -30,6 +30,9 @@ local plugins = {
 	-- <C-a> and <C-x> now work better with dates
 
 	{'tpope/vim-endwise' } -- auto-add 'fi' after 'if', and similar
+
+	'tpope/vim-fugitive' -- git plugin
+	-- :Git for git commands with some nice wrappers
 }
 
 require("lazy").setup(plugins)
