98fa3892583382d4e7d2341b4caa85996eadf293
Jake <jacobmcpeak98@gmail.com>
2024-06-12 22:22:53 -0400

    Add fzf mapping to use git files when in a repo

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -1,3 +1,16 @@
 local map = vim.api.nvim_set_keymap
 
+function SFiles() -- smart Files. uses git if in git repo
+	vim.cmd([[
+		let out = system('git rev-parse --is-inside-work-tree')
+		if stridx(out, 'true') >= 0
+			:GFiles
+		else
+			:Files
+		endif
+	]])
+end
+
 map('n', '<leader>b', '<cmd>Buffers<cr>', {})
+
+map('n', '<leader>o', '<cmd>lua SFiles()<cr>', {})
