3ebc2fbbbc37f1f018121493e210f627a88a0cfd
Boodleboy <jacobmcpeak98@gmail.com>
2025-10-02 07:10:49 -0400

    Disable auto-pairs and news

--- nvim/lua/plugins/disabled.lua
+++ nvim/lua/plugins/disabled.lua
@@ -1,6 +1,8 @@
 return {
-  { "Saghen/blink.cmp",  enabled = false },
-  { "ggandor/leap.nvim", enabled = false },
-  { "ggandor/flit.nvim", enabled = false },
-  { "folke/flash.nvim",  enabled = false },
+  { "Saghen/blink.cmp",     enabled = false },
+  { "ggandor/leap.nvim",    enabled = false },
+  { "ggandor/flit.nvim",    enabled = false },
+  { "folke/flash.nvim",     enabled = false },
+  { "nvim-mini/mini.pairs", enabled = false },
+  news = { lazyvim = false }
 }
